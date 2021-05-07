8f7abd748942e46f1ac20d2a44e2c8d57eeada72
G100my <G100my.lan229@gmail.com>
2021-05-07 23:23:20 +0800

    init editor

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <HelloWorld />
   </div>
 </template>
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
 
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
